d7738b5039f4c6939883a0d8577d3994f2f3f0d1
Ks Arjun <kosgiarjun@gmail.com>
2026-05-03 18:43:48 +0530

    Initial Commit

---
+++ LabLens/LabLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabLensApp.swift
+//  LabLens
+//
+//  Created by Apple  on 03/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LabLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
